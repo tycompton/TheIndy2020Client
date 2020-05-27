f6a03ffcbd61eec2393c0cad6f50493a9f405e40
tycompton <tyrone.compton@gmail.com>
2020-05-27 23:54:27 +0100

    Add category created

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,11 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 import Home from './core/Home';
+import PrivateRoute from './auth/PrivateRoute';
+import Dashboard from './user/UserDashboard';
+import AdminRoute from './auth/AdminRoute';
+import AdminDashboard from './user/AdminDashboard';
+import AddCategory from './admin/AddCategory';
 
 const Routes = () => {
   return (
@@ -11,8 +16,11 @@ const Routes = () => {
         <Route path="/" exact component={Home} />
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
+        <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
+        <AdminRoute path="/create/category" exact component={AddCategory} />
       </Switch>
-    </BrowserRouter>
+    </BrowserRouter> 
   );
 };
 
